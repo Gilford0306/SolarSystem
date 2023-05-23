cce99db7948dc8d3d058e8c63ee0116a8aeffac4
Gilford0306 <Gilford0306@gmail.com>
2023-05-23 15:47:37 +0300

    ADD moons + resorse file

--- SolarSystem/Form1.Designer.cs
+++ SolarSystem/Form1.Designer.cs
@@ -37,9 +37,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(825, 28);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(806, 35);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(223, 61);
             this.button1.TabIndex = 0;
             this.button1.Text = "strart";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,14 +49,16 @@
             // 
             // timer1
             // 
-            this.timer1.Interval = 200;
+            this.timer1.Interval = 500;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(825, 71);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(806, 143);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(223, 61);
             this.button2.TabIndex = 1;
             this.button2.Text = "stop";
             this.button2.UseVisualStyleBackColor = true;
@@ -62,18 +66,18 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(928, 744);
+            this.ClientSize = new System.Drawing.Size(1069, 1016);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Solar system";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
 
--- SolarSystem/Form1.cs
+++ SolarSystem/Form1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace SolarSystem
@@ -22,7 +24,12 @@ namespace SolarSystem
         float angelnep = 0, nep_velosity = 0.006F;
 
 
-        int px = 350, py = 300, dst = 50;
+        int px = 500, py = 500, dst = 50;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -45,61 +52,116 @@ namespace SolarSystem
             Graphics g = e.Graphics;
             g.SmoothingMode = SmoothingMode.AntiAlias;
             g.TranslateTransform(px, py, MatrixOrder.Append);
-            g.DrawEllipse(Pens.White, -95, -100, 200, 200);
+            g.DrawEllipse(Pens.White, -140, -150, 300, 300);
             g.ResetTransform();
 
 
 
             //g.FillEllipse(Brushes.Yellow, px-6, py-10, 25, 25);
-            Image Sun= Image.FromFile(@"D:\Projects\5.WinForms\SolarSystem\SolarSystem\sun.png");
-            e.Graphics.DrawImage(Sun, px - 6, py - 10, 25, 25);
 
-            Image Mercyri = Image.FromFile(@"D:\Projects\5.WinForms\SolarSystem\SolarSystem\Mercyri.png");      
+            Image Sun = Properties.Resources.sun;
+            e.Graphics.DrawImage(Sun, px - 4, py - 9, 25, 25);
+            Image Mercyri = Properties.Resources.Mercyri;
             int x = distanseX(20, angelmerc);
             int y = distanseY(20, angelmerc);
             e.Graphics.DrawImage(Mercyri, (int)x, (int)y, 10, 10);
 
-            Image Ven= Image.FromFile(@"D:\Projects\5.WinForms\SolarSystem\SolarSystem\Ven.png");
-            x = r_distanseX(40, angelven) ;
-            y = r_distanseY(40, angelven) ;
+            Image Ven = Properties.Resources.Ven;
+            x = r_distanseX(40, angelven);
+            y = r_distanseY(40, angelven);
             //g.FillEllipse(Brushes.Orange, (int)x, (int)y, 10, 10);
             e.Graphics.DrawImage(Ven, (int)x, (int)y, 10, 10);
 
-            Image Earht = Image.FromFile(@"D:\Projects\5.WinForms\SolarSystem\SolarSystem\earth.png");
-            x = distanseX(60, angelearth);
-            y = distanseY(60, angelearth);
+            Image Earht = Properties.Resources.earth;
+            x = distanseX(80, angelearth);
+            y = distanseY(80, angelearth);
             //g.FillEllipse(Brushes.Aqua, (int)x, (int)y, 10, 10);
+
             e.Graphics.DrawImage(Earht, (int)x, (int)y, 10, 10);
 
-            Image Mars = Image.FromFile(@"D:\Projects\5.WinForms\SolarSystem\SolarSystem\mars.png");
-            x = distanseX(80, angelmars);
-            y = distanseY(80, angelmars);
+            //g.DrawEllipse(Pens.White, (int)x - 10, (int)y - 10, 30, 30);
+            int x1 = (int)(x + 15 * Math.Sin(angelearth * Math.PI));
+            int y1 = (int)(y + 15 * Math.Cos(angelearth * Math.PI));
+            g.FillEllipse(Brushes.Wheat, (int)x1, (int)y1, 5, 5);
+
+            Image Mars = Properties.Resources.mars;
+            x = distanseX(120, angelmars);
+            y = distanseY(120, angelmars);
             //g.FillEllipse(Brushes.Red, (int)x, (int)y, 10, 10);
             e.Graphics.DrawImage(Mars, (int)x, (int)y, 10, 10);
+            x1 = (int)(x + 15 * Math.Sin(angelmars * Math.PI+1f));
+            y1 = (int)(y + 15 * Math.Cos(angelmars * Math.PI + 1F));
+            int x2 = (int)(x + 20 * Math.Sin(angelmars * Math.PI));
+            int y2 = (int)(y + 20 * Math.Cos(angelmars * Math.PI));
+            g.FillEllipse(Brushes.Wheat, (int)x1, (int)y1, 5, 5);
+            g.FillEllipse(Brushes.Wheat, (int)x2, (int)y2, 5, 5);
 
-            x = distanseX(120, angeljup);
-            y = distanseY(120, angeljup);
-            Image JUPITER = Image.FromFile(@"D:\Projects\5.WinForms\SolarSystem\SolarSystem\JUPITER.png");
+            x = distanseX(200, angeljup);
+            y = distanseY(200, angeljup);
+            Image JUPITER = Properties.Resources.mars;
             //g.FillEllipse(Brushes.Brown, (int)x, (int)y, 18, 18);
             e.Graphics.DrawImage(JUPITER, (int)x, (int)y, 18, 18);
+            x1 = (int)(x + 35 * Math.Sin(angelmars * Math.PI + 2F));
+            y1 = (int)(y + 35 * Math.Cos(angelmars * Math.PI+2F));
+            x2 = (int)(x + 20 * Math.Sin(angelmars * Math.PI));
+            y2 = (int)(y + 20 * Math.Cos(angelmars * Math.PI));
+            g.FillEllipse(Brushes.Wheat, (int)x1, (int)y1, 8, 8);
+            g.FillEllipse(Brushes.Wheat, (int)x2, (int)y2, 5, 5);
+            x1 = (int)(x + 25 * Math.Sin(angelmars * Math.PI));
+            y1 = (int)(y + 25 * Math.Cos(angelmars * Math.PI));
+            x2 = (int)(x + 30 * Math.Sin(angelmars * Math.PI- 2f));
+            y2 = (int)(y + 30 * Math.Cos(angelmars * Math.PI - 2f));
+            g.FillEllipse(Brushes.Wheat, (int)x1, (int)y1, 4, 4);
+            g.FillEllipse(Brushes.Wheat, (int)x2, (int)y2, 7, 7);
 
-            Image Sat = Image.FromFile(@"D:\Projects\5.WinForms\SolarSystem\SolarSystem\sat.png");
-            x = distanseX(150, angelsat);
-            y = distanseY(150, angelsat);
+
+            Image Sat = Properties.Resources.sat;
+            x = distanseX(280, angelsat);
+            y = distanseY(280, angelsat);
             //g.FillEllipse(Brushes.Purple, (int)x, (int)y, 15, 15);
             e.Graphics.DrawImage(Sat, (int)x, (int)y, 15, 15);
+            x1 = (int)(x + 35 * Math.Sin(angelmars * Math.PI + 3F));
+            y1 = (int)(y + 35 * Math.Cos(angelmars * Math.PI + 3F));
+            x2 = (int)(x + 20 * Math.Sin(angelmars * Math.PI));
+            y2 = (int)(y + 20 * Math.Cos(angelmars * Math.PI));
+            g.FillEllipse(Brushes.Wheat, (int)x1, (int)y1, 8, 8);
+            g.FillEllipse(Brushes.Wheat, (int)x2, (int)y2, 5, 5);
+            x1 = (int)(x + 25 * Math.Sin(angelmars * Math.PI));
+            y1 = (int)(y + 25 * Math.Cos(angelmars * Math.PI));
+            x2 = (int)(x + 30 * Math.Sin(angelmars * Math.PI - 1f));
+            y2 = (int)(y + 30 * Math.Cos(angelmars * Math.PI - 1f));
+            g.FillEllipse(Brushes.Wheat, (int)x1, (int)y1, 4, 4);
+            g.FillEllipse(Brushes.Wheat, (int)x2, (int)y2, 7, 7);
 
-            Image uranus = Image.FromFile(@"D:\Projects\5.WinForms\SolarSystem\SolarSystem\uranus.png");
-            x = r_distanseX(180, angelur);
-            y = r_distanseY(180, angelur);
+            Image uranus = Properties.Resources.uranus;
+            x = r_distanseX(350, angelur);
+            y = r_distanseY(350, angelur);
             //g.FillEllipse(Brushes.Blue, (int)x, (int)y, 13, 13);
             e.Graphics.DrawImage(uranus, (int)x, (int)y, 13, 13);
+            x1 = (int)(x + 35 * Math.Sin(angelmars * Math.PI + 2F));
+            y1 = (int)(y + 35 * Math.Cos(angelmars * Math.PI + 2F));
+            x2 = (int)(x + 20 * Math.Sin(angelmars * Math.PI));
+            y2 = (int)(y + 20 * Math.Cos(angelmars * Math.PI));
+            g.FillEllipse(Brushes.Wheat, (int)x1, (int)y1, 8, 8);
+            g.FillEllipse(Brushes.Wheat, (int)x2, (int)y2, 5, 5);
+            x1 = (int)(x + 25 * Math.Sin(angelmars * Math.PI));
+            y1 = (int)(y + 25 * Math.Cos(angelmars * Math.PI));
+            x2 = (int)(x + 30 * Math.Sin(angelmars * Math.PI - 3));
+            y2 = (int)(y + 30 * Math.Cos(angelmars * Math.PI - 3f));
+            g.FillEllipse(Brushes.Wheat, (int)x1, (int)y1, 4, 4);
+            g.FillEllipse(Brushes.Wheat, (int)x2, (int)y2, 5, 5);
 
-            Image nep = Image.FromFile(@"D:\Projects\5.WinForms\SolarSystem\SolarSystem\nep.png");
-            x = r_distanseX(210, angelnep);
-            y = r_distanseY(210, angelnep);
+            Image nep = Properties.Resources.nep;
+            x = r_distanseX(430, angelnep);
+            y = r_distanseY(430, angelnep);
             g.FillEllipse(Brushes.Purple, (int)x, (int)y, 15, 15);
             e.Graphics.DrawImage(nep, (int)x, (int)y, 15, 15);
+            x1 = (int)(x + 35 * Math.Sin(angelmars * Math.PI + 2F));
+            y1 = (int)(y + 35 * Math.Cos(angelmars * Math.PI + 2F));
+            x2 = (int)(x + 20 * Math.Sin(angelmars * Math.PI));
+            y2 = (int)(y + 20 * Math.Cos(angelmars * Math.PI));
+            g.FillEllipse(Brushes.Wheat, (int)x1, (int)y1, 8, 8);
+            g.FillEllipse(Brushes.Wheat, (int)x2, (int)y2, 5, 5);
 
             angelmerc -= mecr_velosity;
             angelven -= ven_velosity;
